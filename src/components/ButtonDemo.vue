d1ecbdd1e27bc39010d658c0a9e0dca072efa2ab
xsnowholy <495904127@qq.com>
2021-01-16 23:58:41 +0800

    完成基础类型、大小类型和加载中样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,20 +2,45 @@
   <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
-    <Button>您好</Button>
-    <Button theme="primary">您好</Button>
-    <Button theme="link">您好</Button>
-    <Button theme="text">您好</Button>
+    <Button theme="primary">普通</Button>
+    <Button>普通</Button>
+    <Button theme="link">普通</Button>
+    <Button theme="text">普通</Button>
+  </div>
+  <h1>示例2</h1>
+  <div>
+    <Button theme="primary">普通</Button>
+    <Button>普通</Button>
+    <Button theme="link">普通</Button>
+    <Button theme="text">普通</Button>
+  </div>
+  <br />
+  <div>
+    <Button theme="primary" size="big">大按钮</Button>
+    <Button size="big">大按钮</Button>
+    <Button theme="link" size="big">大按钮</Button>
+    <Button theme="text" size="big">大按钮</Button>
+  </div>
+  <br />
+  <div>
+    <Button theme="primary" size="small">小按钮</Button>
+    <Button size="small">小按钮</Button>
+    <Button theme="link" size="small">小按钮</Button>
+    <Button theme="text" size="small">小按钮</Button>
+  </div>
+  <br />
+  <div>
+    <Button theme="primary" loading>加载中</Button>
   </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
+import Button from '../lib/Button.vue';
 
 export default {
   name: 'ButtonDemo',
   components: {
     Button,
   },
-}
+};
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,10 +1,13 @@
 <template>
-  <button class="taiji-button" :class="`taiji-theme-${theme}`">
+  <button class="taiji-button" :class="classes">
+    <span :class="{'taiji-button-loading': loading}"></span>
     <slot />
   </button>
 </template>
 
 <script lang="ts">
+import { computed } from 'vue';
+
 export default {
   name: 'Button',
   props: {
@@ -12,21 +15,58 @@ export default {
       type: String,
       default: 'button',
     },
+    size: {
+      type: String,
+      default: 'normal',
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
   },
-}
+  setup(props) {
+    const { theme, size } = props;
+    const classes = computed(() => {
+      return {
+        [`taiji-theme-${theme}`]: theme,
+        [`taiji-theme-${size}`]: size,
+      };
+    });
+
+    return {
+      classes,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
 $h: 32px;
+$bigH: 48px;
+$bigSize: 18px;
+$smallSize: 14px;
+$smallH: 28px;
 $border-color: #333;
+$normalTextColor: #333;
+$linkTextColor: #3393ec;
+$textColor: #333;
 $color: #fff;
+$borderPx: 4px;
 $gray: #a8a8a8;
 $radius: 4px;
 $bg: #333;
+$normalBg: #fff;
+
+@keyframes rotateAnimation {
+  100% {
+    transform: rotate(360deg);
+  }
+}
 
 .taiji-button {
   box-sizing: border-box;
   height: $h;
+  font-size: 14px;
   line-height: $h;
   padding: 0 12px;
   cursor: pointer;
@@ -34,9 +74,9 @@ $bg: #333;
   justify-content: center;
   align-items: center;
   white-space: nowrap;
-  background: $bg;
-  color: $color;
-  border: 6px solid $border-color;
+  background: $normalBg;
+  color: $normalTextColor;
+  border: $borderPx solid $border-color;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
 
   & + & {
@@ -45,8 +85,8 @@ $bg: #333;
 
   &:hover,
   &:focus {
-    border-color: lighten($bg, 20%);
-    background: lighten($bg, 20%);
+    border-color: lighten($border-color, 20%);
+    background: lighten($normalBg, 20%);
   }
 
   &:focus {
@@ -55,6 +95,66 @@ $bg: #333;
 
   &::-moz-focus-inner {
     border: 0;
+  }
+
+  &.taiji-theme-big {
+    height: $bigH;
+    font-size: $bigSize;
+    padding: 0 18px;
+  }
+
+  &.taiji-theme-small {
+    height: $smallH;
+    font-size: $smallSize;
+    padding: 0 10px;
+  }
+
+  &.taiji-theme-primary {
+    background: $bg;
+    color: $color;
+
+    &:hover,
+    &:focus {
+      border-color: lighten($bg, 20%);
+      background: lighten($bg, 20%);
+    }
+  }
+
+  &.taiji-theme-link {
+    background: transparent;
+    border-color: transparent;
+    box-shadow: none;
+    color: $linkTextColor;
+
+    &:hover,
+    &:focus {
+      color: lighten($linkTextColor, 10%);
+    }
+  }
+
+  &.taiji-theme-text {
+    background: transparent;
+    border-color: transparent;
+    box-shadow: none;
+    color: $textColor;
+
+    &:hover,
+    &:focus {
+      background: #c0c0c0;
+    }
+  }
+
+  > .taiji-button-loading {
+    display: block;
+    margin-right: 6px;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background: transparent;
+    border: 3px solid transparent;
+    border-top: 3px solid #e2e2e2;
+    border-left: 3px solid #e2e2e2;
+    animation: rotateAnimation infinite linear 1.2s;
   }
 }
 </style>
